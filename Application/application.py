c714ebb827161b5c82222ae905705a74b3e45c20
Vansh <Vansh>
2024-06-12 22:34:37 +0530

    Added optional deckle feature

--- Application/application.py
+++ Application/application.py
@@ -23,7 +23,7 @@ def upload_file():
             dfs = split_dataframe(df)
             for name, df_group in dfs.items():
                 # second parameter is boolean to decide whether minimum waste or minimum changes is needed
-                completed_dict, plan_df = optimise_deckle(df_group, 0)
+                completed_dict, plan_df = optimise_deckle(df_group, 1)
                 customer_df = customer_table(completed_dict,df_group)
             return jsonify({'message': 'File processed successfully', 'data': data}), 200
         except Exception as e:
