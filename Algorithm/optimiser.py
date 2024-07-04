ac75cd0ed99413dd1a42bb165bac566bac01b2c1
Vansh <Vansh>
2024-07-04 21:43:38 +0530

    Added key metrics data

--- Algorithm/optimiser.py
+++ Algorithm/optimiser.py
@@ -5,15 +5,17 @@ from Algorithm.wastage_optimiser import get_optimised_wastage_deckle
 
 
 def get_optimised_knives(client_metadata, data):
-    plan_df, customer_df = common_optimisation_logic(data, get_optimised_knives_deckle)
+    plan_df, customer_df, metric_df = common_optimisation_logic(data, get_optimised_knives_deckle)
     save_results_to_file(client_metadata, plan_df, 'knives_optimisation', 'planning_output')
     save_results_to_file(client_metadata, customer_df, 'knives_optimisation', 'customer_output')
+    save_results_to_file(client_metadata, metric_df, 'knives_optimisation', 'metrics_output')
 
 
 def get_optimised_wastage(client_metadata, data):
-    plan_df, customer_df = common_optimisation_logic(data, get_optimised_wastage_deckle)
+    plan_df, customer_df, metric_df = common_optimisation_logic(data, get_optimised_wastage_deckle)
     save_results_to_file(client_metadata, plan_df, 'wastage_optimisation', 'planning_output')
     save_results_to_file(client_metadata, customer_df, 'wastage_optimisation', 'customer_output')
+    save_results_to_file(client_metadata, metric_df, 'wastage_optimisation', 'metrics_output')
 
 
 def optimise_deckle(client_metadata, data):
--- Database/s3_operations.py
+++ Database/s3_operations.py
@@ -13,7 +13,6 @@ def upload_to_s3(file_name, folder_name):
     s3_key = f'{folder_name}/{os.path.basename(file_name)}'
     s3_client.upload_file(file_name, Storage.s3_bucket, s3_key)
 
-
 def read_from_s3(client_name, product_name, product_type, algorithm_name, result_type):
     s3_key = f'{client_name}/{product_name}/{product_type}/{algorithm_name}/{algorithm_name}_{result_type}_results.csv'
     response = s3_client.get_object(Bucket=Storage.s3_bucket, Key=s3_key)
@@ -42,18 +41,22 @@ def save_results_to_file(client_metadata, data, algorithm_name, result_type):
 def get_knives_results(client_name, product_name, product_type):
     plan_json = read_from_s3(client_name, product_name, product_type, 'knives_optimisation', 'planning_output')
     customer_json = read_from_s3(client_name, product_name, product_type, 'knives_optimisation', 'customer_output')
+    metric_json = read_from_s3(client_name, product_name, product_type, 'knives_optimisation', 'metrics_output')
 
     customer_json = parse_csv_to_json(customer_json)
     plan_json = parse_csv_to_json(plan_json)
+    metric_json = parse_csv_to_json(metric_json)
 
-    return {'plan': plan_json, 'customer': customer_json}
+    return {'plan': plan_json, 'customer': customer_json, 'metric': metric_json}
 
 
 def get_wastage_results(client_name, product_name, product_type):
     plan_json = read_from_s3(client_name, product_name, product_type, 'wastage_optimisation', 'planning_output')
     customer_json = read_from_s3(client_name, product_name, product_type, 'wastage_optimisation', 'customer_output')
+    metric_json = read_from_s3(client_name, product_name, product_type, 'wastage_optimisation', 'metrics_output')
 
     customer_json = parse_csv_to_json(customer_json)
     plan_json = parse_csv_to_json(plan_json)
+    metric_json = parse_csv_to_json(metric_json)
 
-    return {'plan': plan_json, 'customer': customer_json}
+    return {'plan': plan_json, 'customer': customer_json, 'metric': metric_json}
