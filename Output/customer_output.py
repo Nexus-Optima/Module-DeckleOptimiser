3ba437355daf6cbf753b53251c47f8a4b313a790
Vansh <Vansh>
2024-08-08 12:52:18 +0530

    Modified logic to get correct SO quantity and pending quantity

--- Output/customer_output.py
+++ Output/customer_output.py
@@ -8,7 +8,7 @@ def customer_table(final_dict, df):
         [OrderDetails.item, OrderDetails.sales_order, OrderDetails.customer_name, OrderDetails.consignee_name, OrderDetails.width, OrderDetails.product_ID, OrderDetails.number_of_rolls, Optional.column_name]]
     customer_df[CustomerTable.actual_rolls] = None
     customer_df[CustomerTable.target_rolls] = np.ceil(customer_df[CustomerTable.target_rolls])
-    customer_df[CustomerTable.target_quantity] = df[OrderDetails.product_qty].round(0).astype(int)
+    customer_df[CustomerTable.target_quantity] = df[OrderDetails.so_qty].round(0).astype(int)
     for i in range(len(customer_df)):
         if customer_df[Optional.column_name].iloc[i] == Optional.must_make:
             if customer_df[CustomerTable.width].iloc[i] in final_dict.keys():
@@ -30,7 +30,7 @@ def customer_table(final_dict, df):
         else:
             continue
 
-    customer_df[CustomerTable.production_quantity] = customer_df[CustomerTable.actual_rolls] * customer_df[CustomerTable.width] * df[OrderDetails.product_thickness] * df[OrderDetails.product_length] * 0.91/ 1000000
+    customer_df[CustomerTable.production_quantity] = (customer_df[CustomerTable.actual_rolls] * customer_df[CustomerTable.width] * df[OrderDetails.product_thickness] * df[OrderDetails.product_length] * 0.91/ 1000000) + df[OrderDetails.current_stock]
     customer_df[CustomerTable.production_quantity] = customer_df[CustomerTable.production_quantity].astype(float).round(2)
     customer_df[CustomerTable.pending_quantity] = customer_df[CustomerTable.target_quantity] - customer_df[CustomerTable.production_quantity]
     return customer_df
